7930ed14ee4da628bed6dd0ac3614cf4a9f44569
kerimovaselime02@gmail.com <kerimovaselime02@gmail.com>
2023-10-30 22:09:24 +0400

    init

---
+++ 01-data-type/main.js
@@ -0,0 +1,59 @@
+// Task 1
+let firstName='Selime';
+let lastName='Kerimova';
+let fullName;
+fullName=firstName+' '+lastName;
+console.log(fullName);
+
+
+let job='teacher.';
+let age=21;
+let world='Azerbaycan.';
+let about;
+about=console.log(fullName+' '+'I am a '+job+'I live in '+world+'I am '+age+' years old');
+
+
+// Task 2
+let lenght= 16;
+let color='brown';
+let float='12.5';
+let like=true;
+let x={
+    firstName:'Selime',
+    color:'blue'
+}
+console.log(typeof lenght);
+console.log(typeof color);
+console.log(typeof float);
+console.log(typeof like);
+console.log(typeof x);
+
+
+// Task 3
+let road=8600;
+let speed=120;
+let time;;
+time=road/speed;
+console.log(time)
+
+// Task 4
+let yearOfBirth=2002;
+let currentYear=2023;
+let old;
+old=currentYear-yearOfBirth;
+console.log(old);
+
+// Task  5
+let number=8;
+let selime=100;
+let nurlana=100;
+let fidan =100;
+let nezrin=100;
+let nergiz=100;
+let ramile=100;
+let aydan=20;
+let aytac=20;
+let average;
+average=(selime+nurlana+fidan+nezrin+nergiz+ramile+aydan+aytac)/8;
+console.log('Telebelerin ortalama bali:'+average)
+
