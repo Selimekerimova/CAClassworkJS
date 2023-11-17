fc1ba3c3da2f511a653ef1fb77e76bf165cfbfc1
kerimovaselime02@gmail.com <kerimovaselime02@gmail.com>
2023-11-18 01:45:14 +0400

    fix

--- task/main.js
+++ task/main.js
@@ -70,13 +70,88 @@
 
 
 //TASK7
-const company = [
-  { firstName: "John", lastName: "Doe", salary: 50000, yearsWorked: 5 },
-  { firstName: "Alice", lastName: "Johnson", salary: 60000, yearsWorked: 8 },
-  { firstName: "Bob", lastName: "Smith", salary: 75000, yearsWorked: 3 },
-  { firstName: "Eve", lastName: "Williams", salary: 55000, yearsWorked: 7 },
+// const company = [
+//   { firstName: "John", lastName: "Doe", salary: 50000, yearsWorked: 5 },
+//   { firstName: "Alice", lastName: "Johnson", salary: 60000, yearsWorked: 8 },
+//   { firstName: "Bob", lastName: "Smith", salary: 75000, yearsWorked: 3 },
+//   { firstName: "Eve", lastName: "Williams", salary: 55000, yearsWorked: 7 },
+// ];
+// let str=company.map((item)=>{
+//     return `${item.firstName} ${item.lastName} illik ${item.salary} maas alir ve ${item.yearsWorked} il calisir`
+// })
+// console.log(str)
+
+
+
+////////
+// Verilmiş array-in ən uzun sözünü ekrana çıxaran proqram yazın
+
+// let array = [
+//   "Facebook",
+//   "Google",
+//   "Microsoft",
+//   "Apple",
+//   "IBM",
+//   "Oracle",
+//   "Amazon",
+// ];
+// let max=array[0].length;
+// let maxIndex=0;
+// function maxLength(arr){
+//   arr.forEach((element,i) => {
+//     if(element.length>max){
+//       max=element
+//       maxIndex=element[i]
+//     }
+//   });
+//   return max
+// }
+// console.log(maxLength(array))
+
+
+////////
+const products = [
+  { name: "one Product 1", price: 20, category: "Electronics" },
+  { name: "two Product 2", price: 30, category: "Clothes" },
+  { name: "three Product 3", price: 40, category: "Electronics" },
+  { name: "four Product 4", price: 50, category: "Clothes" },
+  { name: "five Product 5", price: 60, category: "Clothes" },
+  { name: "six Product 6", price: 70, category: "Electronics" },
+  { name: "seven Product 7", price: 80, category: "Clothes" },
+  { name: "eight Product 8", price: 90, category: "electronics" },
 ];
-let str=company.map((item)=>{
-    return `${item.firstName} ${item.lastName} illik ${item.salary} maas alir ve ${item.yearsWorked} il calisir`
-})
-console.log(str)
+
+//only price more tha 50 objects
+// console.log(products.filter((item) => {
+//  return item.price>50?item:null
+// }));
+
+
+
+//50 dən böyük və catagory elektroniks olanlar
+// console.log(
+//   products.filter((item) => item.price > 50 && item.category.toLocaleLowerCase() === 'electronics' ? item : null)
+// );
+
+
+// 50 dən böyük və catagory "c" ilə başlayan
+// console.log(products.filter((item) => item.price > 50 && item.category.startsWith('C') ? item : null))
+
+
+////////////////////
+//Verilmiş array-in bütün hərfləri böyük olan sözün özünü və indeksini çapa çıxaran proqram yazın.
+
+let companies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "APPLE",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+console.log(
+companies.filter((item,i)=>item===item.toLocaleUpperCase()?item:null)
+)
+// 3. APPLE
+// 4.IBM
