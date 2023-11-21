fc023e80a242062432955b2db3280c6eb3b6ea71
Kerimovaselime <kerimovaselime02@gmail.com>
2023-11-21 13:41:59 +0400

    init

---
+++ 21.11.2023/main.js
@@ -0,0 +1,40 @@
+//task 1
+// let button=document.querySelector('.button');
+// button.addEventListener('click',function(){
+// this.parentElement.style.width='200px';
+// this.parentElement.style.height='200px';
+// this.parentElement.style.backgroundColor='green';
+// this.parentElement.style.position='relative';
+// this.style.position='absolute'
+// this.style.right='20px'
+// this.style.bottom='20px'
+// })
+
+//Task 2
+// let city=document.querySelector('.city');
+// let list=document.createElement('ul')
+// city.append(list)
+// const liArr=['Goranboy','Baku','Ganja','Yevlakh','Shamkir']
+
+
+//  liArr.forEach((elem)=>{
+//     let liElem  =document.createElement('li')
+//     liElem.innerText=elem
+//     list.append(liElem)
+// liElem.addEventListener('click', function(){
+//     this.remove()
+// })
+// })
+
+//Task 3
+
+let nvb=document.querySelector('nav')
+nvb.style.display='flex'
+nvb.style.justifyContent='sp'
+let h1Title=document.querySelector('.title')
+
+//Task 4
+// let toggle = document.querySelector(".toggle");
+// toggle.addEventListener('click',function(){
+//    document.body.classList.toggle("theme");
+// })
